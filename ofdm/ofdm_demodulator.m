b3d5f3ce53c1f6e7b1b4c695e58db5ee99702d58
Sarvesh Shiva Sharan <shivasharan2004@gmail.com>
2025-12-31 00:12:25 +0530

    Add OFDM demodulator with CP removal and FFT

--- ofdm/ofdm_demodulator.m
+++ ofdm/ofdm_demodulator.m
@@ -1,10 +1,31 @@
-function ddRx = otfs_demodulator(rxSignal, N, K)
-% OTFS DEMODULATOR
-% Time domain → TF domain → DD domain
+function rxBits = ofdm_demodulator(rxSignal, M, Nsub)
+% ----------------------------------------
+% OFDM DEMODULATOR
+% rxSignal : received time-domain OFDM signal
+% M        : QAM order
+% Nsub     : number of subcarriers
+% ----------------------------------------
 
-    % Wigner transform: Time → TF
-    tfRx = wigner(rxSignal, N, K);
+cpLen = Nsub/4;
+symLen = Nsub + cpLen;
 
-    % SFFT: TF → Delay-Doppler
-    ddRx = sfft(tfRx);
+% Reshape received signal
+numSymbols = floor(length(rxSignal)/symLen);
+rxSignal = rxSignal(1:numSymbols*symLen);
+rxMatrix = reshape(rxSignal, symLen, numSymbols);
+
+% Remove cyclic prefix
+rxNoCP = rxMatrix(cpLen+1:end, :);
+
+% FFT
+fftData = fft(rxNoCP, Nsub, 1);
+
+% Parallel to serial
+rxQAM = fftData(:);
+
+% QAM demodulation
+rxBits = qamdemod(rxQAM, M, ...
+    'OutputType','bit', ...
+    'UnitAveragePower', true);
+
 end
