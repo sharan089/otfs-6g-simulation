346181c03d8f3d6a5cc99bb3aed2c8b172197a7f
Sarvesh Shiva Sharan <shivasharan2004@gmail.com>
2025-12-31 00:12:25 +0530

    Add OFDM modulator with cyclic prefix

--- ofdm/ofdm_modulator.m
+++ ofdm/ofdm_modulator.m
@@ -1,10 +1,30 @@
-function txSignal = otfs_modulator(ddGrid)
-% OTFS MODULATOR
-% DD domain → TF domain → Time domain
+function tx_ofdm = ofdm_modulator(dataBits, M, Nsub)
+% ----------------------------------------
+% OFDM MODULATOR
+% dataBits : input bit stream
+% M        : QAM order (e.g., 4 for QPSK)
+% Nsub     : number of subcarriers
+% ----------------------------------------
 
-    % ISFFT: Delay-Doppler → Time-Frequency
-    tfGrid = isfft(ddGrid);
+% QAM mapping
+qamSymbols = qammod(dataBits, M, ...
+    'InputType','bit', ...
+    'UnitAveragePower', true);
 
-    % Heisenberg transform: TF → Time
-    txSignal = heisenberg(tfGrid);
+% Serial to parallel
+numSymbols = floor(length(qamSymbols)/Nsub);
+qamSymbols = qamSymbols(1:numSymbols*Nsub);
+ofdmGrid = reshape(qamSymbols, Nsub, numSymbols);
+
+% IFFT
+ifftData = ifft(ofdmGrid, Nsub, 1);
+
+% Add cyclic prefix (25%)
+cpLen = Nsub/4;
+cp = ifftData(end-cpLen+1:end, :);
+ofdmWithCP = [cp; ifftData];
+
+% Parallel to serial
+tx_ofdm = ofdmWithCP(:);
+
 end
