dd516aee0022a67ef74fc9782c174e5ca96c13e2
Sarvesh Shiva Sharan <shivasharan2004@gmail.com>
2025-12-30 23:42:12 +0530

    Add Heisenberg transform for OTFS transmitter

--- transmitter/heisenberg.m
+++ transmitter/heisenberg.m
@@ -1,7 +1,11 @@
 function tx = heisenberg(X_tf)
 % HEISENBERG TRANSFORM
-% Converts time-frequency grid to time-domain signal
+% Converts time-frequency grid to time-domain OTFS signal
 
-    tx = ifft(X_tf, [], 1);   % IFFT over time dimension
-    tx = tx(:);               % Serialize signal
+    % IFFT along time axis
+    tx = ifft(X_tf, [], 1);
+
+    % Serialize to 1-D transmit signal
+    tx = tx(:);
+
 end
